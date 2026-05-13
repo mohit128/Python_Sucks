45f13ca2521a47bf292cf20bdaaad237d5ef2e67
mohit128 <budymohitsingh@gmail.com>
2026-05-13 22:40:18 +0530

    Basic Complete

--- GCD.py
+++ GCD.py
@@ -1,6 +1,7 @@
 import math
 def gcd(a,b):
-    if(a==0 or b==0): return mod(a-b) 
+    if(a==0): return b 
+    if b==0: return a
     if(a%b==0):
         return b
     return gcd(b,a%b)
@@ -8,7 +9,7 @@ def gcd(a,b):
 def lcm(a,b):
     if(a==0 or b==0):return 0
     return (a*b)//gcd(a,b)
-print(lcm(12,37))
+print((12,37))
 
 
 
